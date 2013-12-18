bf702ff7e386a11eb4f528953519a1e5d92ce402
Daniele Trapani <daniele.trapani@live.it>
2013-12-18 17:38:40 +0100

    Settings as singleton class

--- app/settings/settings.rb
+++ app/settings/settings.rb
@@ -1,27 +1,24 @@
 require 'yaml'
 
-module Settings
-  class << self
-  @_settings = {}
+class Settings
+  @@_settings = {}
   
-    def load
-      @_settings = YAML.load_file("#{Rails.root}/config/settings.yml")
-    end
-
-    def get(key)
-      @_settings[key.to_s]
-    end
-
-    def set(key, value)
-      @_settings[key.to_s] = value
-      write
-    end
-
-    def write
-      File.open("#{Rails.root}/config/settings.yml", 'w+') do |f|
-        f.write(YAML.dump(@_settings))
-      end
-    end
+  def self.load
+    @@_settings = YAML.load_file("#{Rails.root}/config/settings.yml")
   end
 
+  def self.get(key)
+    @@_settings[key.to_s]
+  end
+
+  def self.set(key, value)
+    @@_settings[key.to_s] = value
+    write
+  end
+
+  def write
+    File.open("#{Rails.root}/config/settings.yml", 'w+') do |f|
+      f.write(YAML.dump(@@_settings))
+    end
+  end
 end
