660ad6b6918e8459e8c510b17e5f41ab01f70393
Daniele Trapani <daniele.trapani@live.it>
2013-12-18 16:50:37 +0100

    Modify Settings as class singleton

--- app/settings/settings.rb
+++ app/settings/settings.rb
@@ -1,26 +1,26 @@
 require 'yaml'
 
 module Settings
-  extend self
-
+  class << self
   @_settings = {}
   
-  def load
-    @_settings = YAML.load_file("#{Rails.root}/config/settings.yml")
-  end
+    def load
+      @_settings = YAML.load_file("#{Rails.root}/config/settings.yml")
+    end
 
-  def get(key)
-    @_settings[key.to_s]
-  end
+    def get(key)
+      @_settings[key.to_s]
+    end
 
-  def set(key, value)
-    @_settings[key.to_s] = value
-    write
-  end
+    def set(key, value)
+      @_settings[key.to_s] = value
+      write
+    end
 
-  def write
-    File.open("#{Rails.root}/config/settings.yml", 'w+') do |f|
-      f.write(YAML.dump(@_settings))
+    def write
+      File.open("#{Rails.root}/config/settings.yml", 'w+') do |f|
+        f.write(YAML.dump(@_settings))
+      end
     end
   end
 
