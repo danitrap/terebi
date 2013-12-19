07c30c9d86678d85f54a5fa7a43e8f009e492ee3
Daniele Trapani <daniele.trapani@live.it>
2013-12-19 17:05:01 +0100

    detect rake and console in initializers

--- config/initializers/delayed_jobs.rb
+++ config/initializers/delayed_jobs.rb
@@ -1,1 +1,4 @@
-UpdateEpisodes.schedule_job
+unless File.basename($0) == "rake" || defined?(Rails::Console)
+  UpdateEpisodes.schedule_job
+  spawn("rake jobs:work")
+end
--- config/initializers/preloads.rb
+++ config/initializers/preloads.rb
@@ -1,6 +1,8 @@
 # preloads calendar data and subtitles availability
-Thread.new {
-  Calendar.today
-  SubtitlesChecker.check!
-  ActiveRecord::Base.connection.close
-}
+unless File.basename($0) == "rake" || defined?(Rails::Console)
+  Thread.new {
+    Calendar.today
+    SubtitlesChecker.check!
+    ActiveRecord::Base.connection.close
+  }
+end
