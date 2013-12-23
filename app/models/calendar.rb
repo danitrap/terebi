8a249233a751663fe2cf8f4fca5a43fe8406f03c
Daniele Trapani <daniele.trapani@live.it>
2013-12-23 01:06:45 +0100

    Better calendar caching

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,10 +2,9 @@ require 'open-uri'
 
 class Calendar
   FEED = "http://services.tvrage.com/feeds/fullschedule.php?country=US"
-  @@today = []
 
   def self.today
-    @@today.empty? ? process_feeds : @@today
+    check_cache
   end
 
   private
@@ -16,21 +15,35 @@ class Calendar
   end
 
   def self.get_feed
-    @@today_us ||= Nokogiri::XML(open(FEED))
+    @us ||= Nokogiri::XML(open(FEED))
   end
 
   def self.parse_feed
-    Time.zone = 'Eastern Time (US & Canada)'
     date = Time.zone.now.strftime("%Y-%m-%d")
-    us_schedule = @@today_us.css("schedule DAY[attr=\"#{date}\"] time")
+    us_schedule = @us.css("schedule DAY[attr=\"#{date}\"] time")
 
-    @@today << create_shows(us_schedule)
-    @@today.flatten!.compact!
+    @today_expires_on = Time.zone.now.midnight + 1.day
+
+    @today = []
+    @today << create_shows(us_schedule)
+    @today.flatten!.compact!
+    @today
   end
 
   def self.create_shows(schedule)
-    schedule.css('show').map do |s|
+  schedule.css('show').map do |s|
       Show.new(s.attr('name'), s.css('title').text, s.parent.attr('attr')) if Series.exists? name: s.attr('name')
+    end
+  end
+
+  def self.check_cache
+    Time.zone = 'Eastern Time (US & Canada)'
+    return process_feeds if @today_expires_on.nil?
+    
+    if Time.zone.now > @today_expires_on
+      process_feeds
+    else
+      @today
     end
   end
 end
