0399f039b840a725ab89553765108b704a4d98d3
Daniele Trapani <daniele.trapani@live.it>
2013-12-25 17:08:47 +0100

    Fix bug in blank state management

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @all_episodes = Episode.all
     @unseen_episodes = Episode.unseen
     @seen_episodes = Episode.seen
-    @seen_count = @seen_episodes.count
-    @unseen_count = @unseen_episodes.count
     @calendar = Calendar.today
   end
 end
