e288ca6d7f7c6265dc08aac9d397b6f2581ba0f4
Daniele Trapani <daniele.trapani@live.it>
2013-12-26 02:22:06 +0100

    Fix bug in loading settings.yml

--- config/initializers/load_settings.rb
+++ config/initializers/load_settings.rb
@@ -1,1 +1,5 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")
+if File.exists? "#{Rails.root}/config/settings.yml"
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")
+else
+  APP_CONFIG = {}
+end
